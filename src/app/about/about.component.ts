3bb0ceb03c84d91cf43e4c88ba828fafc0e84823
reshi-manzoor <manzoor.resie@gmail.com>
2024-06-25 16:56:46 +0530

    added code to about.comonent.html

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    // Initialization logic
+  }
+
+  ngAfterViewInit(): void {
+    // jQuery logic can be safely used here after the view has initialized
+    $(document).ready(() => {
+      $('.skills-list li').hover(
+        function() {
+          $(this).css('color', '#007BFF');
+        },
+        function() {
+          $(this).css('color', '#333');
+        }
+      );
+    });
+  }
 }
